5b8cf57e692a4dfb46ea72a0ebd82704648bc9a5
backfromexile <backfromexile@web.de>
2021-03-29 19:38:26 +0200

    removed trailing dash from storage key base path

--- src/models/ogame/OgameMetaData.ts
+++ src/models/ogame/OgameMetaData.ts
@@ -55,7 +55,7 @@ class OgameMetaData {
 
 
     public get storageKeyPrefix(){
-        return `${this.universeShort}-${this.playerId}-`;
+        return `${this.universeShort}-${this.playerId}`;
     }
 }
 
