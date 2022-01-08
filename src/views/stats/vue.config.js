8ad3333c07f0ac4be1d864d54c60431b9a011ba7
backfromexile <backfromexile@web.de>
2022-01-08 17:47:13 +0100

    set up vue projects for the views

---
+++ src/views/stats/vue.config.js
@@ -0,0 +1,24 @@
+/** @type{import('@vue/cli-service').ProjectOptions} */
+const config = {
+    configureWebpack: {
+        devtool: 'source-map',
+        optimization: {
+            splitChunks: {
+                maxSize: 2_000_000, // Firefox extension requirement
+            },
+        },
+    },
+    css: {
+        loaderOptions: {
+            sass: {
+                // for different styling depending on the browser environment
+                prependData: `
+                    $is-chrome: ${process.env.VUE_APP_BROWSER === 'chrome'};
+                    $is-firefox: ${process.env.VUE_APP_BROWSER === 'firefox'};
+                `
+            },
+        },
+    },
+};
+
+module.exports = config;
