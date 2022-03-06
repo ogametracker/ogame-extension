907dbad8ab020b7393c408b7b3c530116d19de26
backfromexile <backfromexile@web.de>
2022-03-06 12:53:22 +0100

    stats view: registered new ogame components globally

--- src/views/stats/components/common/ogame/index.ts
+++ src/views/stats/components/common/ogame/index.ts
@@ -1,10 +1,16 @@
 import Vue from 'vue';
+import OAllianceClass from './OAllianceClass.vue';
 import OBuilding from './OBuilding.vue';
 import OItem from './OItem.vue';
+import OOfficer from './OOfficer.vue';
+import OPlayerClass from './OPlayerClass.vue';
 import OResource from './OResource.vue';
 import OShip from './OShip.vue';
 
+Vue.component('o-alliance-class', OAllianceClass);
 Vue.component('o-building', OBuilding);
 Vue.component('o-item', OItem);
+Vue.component('o-officer', OOfficer);
+Vue.component('o-player-class', OPlayerClass);
 Vue.component('o-resource', OResource);
 Vue.component('o-ship', OShip);
