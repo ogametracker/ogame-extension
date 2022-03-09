12029a5b96add1cc95fa63ac81538ca2398b3c6a
backfromexile <backfromexile@web.de>
2022-03-09 23:38:44 +0100

    stats view: draft of input styling

--- src/views/stats/views/tools/Index.vue
+++ src/views/stats/views/tools/Index.vue
@@ -1,11 +1,26 @@
 <template>
-    <router-view />
+    <div>
+        <input type="text" v-model="text" />
+        {{ text }}
+        <hr />
+        <input type="number" v-model.number="number" :step="0.1" :min="0" :max="10.1" />
+        {{ number }}
+    </div>
 </template>
 
 <script lang="ts">
-    import { Component, Vue } from 'vue-property-decorator';
+    import { Component, Vue, Watch } from 'vue-property-decorator';
 
     @Component({})
     export default class Index extends Vue {
+        private text = 'Text';
+        private number = 5.55;
+
+        @Watch('number')
+        private onNumberChanged(newValue: string | number) {
+            if(isNaN(this.number) || newValue == '') {
+                this.number = 0;
+            }
+        }
     }
 </script>
