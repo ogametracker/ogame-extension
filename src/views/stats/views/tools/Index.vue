660aeb9de185f9670ef280e3520a07fb147ca07d
backfromexile <backfromexile@web.de>
2022-03-25 11:51:46 +0100

    stats view: added nav items to tools page

--- src/views/stats/views/tools/Index.vue
+++ src/views/stats/views/tools/Index.vue
@@ -1,27 +1,40 @@
 <template>
-    <div>
-        TODO: Tools
-        <!-- <input type="text" v-model="text" />
-        {{ text }}
-        <hr />
-        <input type="number" v-model.number="number" :step="0.1" :min="0" :max="10.1" />
-        {{ number }} -->
-    </div>
+    <page :nav-items="navItems" />
 </template>
 
 <script lang="ts">
-    import { Component, Vue, Watch } from 'vue-property-decorator';
+    import { Component, Vue } from 'vue-property-decorator';
+    import { ListNavItem } from '@stats/components/common/ListNav.vue';
 
     @Component({})
     export default class Index extends Vue {
-        // private text = 'Text';
-        // private number = 5.55;
-
-        // @Watch('number')
-        // private onNumberChanged(newValue: string | number) {
-        //     if(isNaN(this.number) || newValue == '') {
-        //         this.number = 0;
-        //     }
-        // }
+        private get navItems(): ListNavItem[] {
+            return [
+                {
+                    label: 'LOCA: Flight Cost/Time Calculator => Proxy',
+                    to: { name: 'tools/flight' },
+                },
+                {
+                    label: 'LOCA: Cost/Time Calculator => Proxy',
+                    to: { name: 'tools/cost' },
+                },
+                {
+                    label: 'LOCA: Production Calculator => Proxy',
+                    to: { name: 'tools/production' },
+                },
+                {
+                    label: 'LOCA: Trader Resource Conversion => Proxy',
+                    to: { name: 'tools/trader' },
+                },
+                {
+                    label: 'LOCA: Moons => Proxy',
+                    to: { name: 'tools/moons' },
+                },
+                {
+                    label: 'LOCA: Expeditions => ExpoCalc',
+                    to: { name: 'tools/expeditions' },
+                },
+            ];
+        }
     }
 </script>
