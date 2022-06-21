2832b1a809ddd9bd42999e1f477f546675dbb9d6
backfromexile <backfromexile@web.de>
2022-06-21 23:09:00 +0200

    progress player history view

--- src/views/stats/views/universe-history/players/History.vue
+++ src/views/stats/views/universe-history/players/History.vue
@@ -32,7 +32,46 @@
 
             <tabs :tabs="tabs">
                 <template #tab-content-status>
-                    TODO: Status History Graph
+                    <div class="status-history-grid">
+                        <div class="y-ticks">
+                            <span
+                                v-for="(label, i) in statusHistoryLabels"
+                                :key="`status-label-${i}`"
+                                v-text="label"
+                            />
+                        </div>
+
+                        <div class="graph">
+                            <div
+                                :style="{ width: `${statusHistoryWidth}%` }"
+                                style="height: 1px"
+                            />
+                            <div
+                                v-for="(_, i) in statusHistoryLabels"
+                                :key="`status-label-${i}.2`"
+                                class="y-tick"
+                                :style="`--y-tick: ${i}`"
+                            />
+
+                            <div
+                                v-for="(item, i) in statusHistoryItems"
+                                :key="`status-item-${i}`"
+                                :style="{
+                                    left: `${100 * item.start}%`,
+                                    width: `${100 * (item.end - item.start)}%`,
+                                }"
+                                class="bar"
+                                :class="item.class"
+                            />
+                        </div>
+
+                        <div class="x-ticks">
+                            <div
+                                :style="{ width: `${statusHistoryWidth}%` }"
+                                style="height: 1px"
+                            />
+                        </div>
+                    </div>
                 </template>
 
                 <template #tab-content-nickname>
@@ -104,12 +143,13 @@
 </template>
 
 <script lang="ts">
-    import { OgameTrackerUniverseHistoryPlayerAlliance, OgameTrackerUniverseHistoryPlayerName, OgameTrackerUniverseHistoryPlayerState } from '@/shared/db/schema/universe-history';
+    import { DbUniverseHistoryPlayerStateItem, OgameTrackerUniverseHistoryPlayerAlliance, OgameTrackerUniverseHistoryPlayerName, OgameTrackerUniverseHistoryPlayerState } from '@/shared/db/schema/universe-history';
     import { parseIntSafe } from '@/shared/utils/parseNumbers';
     import { GridTableColumn } from '@/views/stats/components/common/GridTable.vue';
     import { Tab } from '@/views/stats/components/common/Tabs.vue';
     import { GlobalOgameMetaData } from '@/views/stats/data/global';
     import { UniverseHistoryDataModule, UniverseHistoryPlayer } from '@/views/stats/data/UniverseHistoryDataModule';
+    import subDays from 'date-fns/subDays';
     import { Component, Prop, Vue } from 'vue-property-decorator';
 
     interface NameHistoryItem {
@@ -122,6 +162,13 @@
         start: number | null;
         end: number | null;
     }
+    interface StatusHistoryItem {
+        status: string | null;
+        start: number;
+        end: number;
+        class: StatusHistoryItemClass;
+    }
+    type StatusHistoryItemClass = 'active' | DbUniverseHistoryPlayerStateItem | 'deleted';
 
     @Component({})
     export default class Players extends Vue {
@@ -249,6 +296,66 @@
             }).reverse();
         }
 
+
+        private readonly statusHistoryTicks = 30;
+
+        private get statusHistoryStartDate(): Date {
+            const nowMinusTicks = subDays(Date.now(), this.statusHistoryTicks - 1);
+            if (this.stateHistory.length == 0) {
+                return nowMinusTicks;
+            }
+
+            const firstDay = new Date(this.stateHistory[0].date);
+            return firstDay < nowMinusTicks ? firstDay : nowMinusTicks;
+        }
+
+        private get statusHistoryItems(): StatusHistoryItem[] {
+            const ticks = this.statusHistoryTicks * 24 * 60 * 60 * 1000;
+            const startDate = this.statusHistoryStartDate.getTime();
+
+            return this.stateHistory.flatMap<StatusHistoryItem>((item, i, history) => {
+                const start = (item.date - startDate) / ticks;
+                const end = ((history[i + 1]?.date ?? Date.now()) - startDate) / ticks;
+
+                if (item.state == 'deleted' || item.state == null) {
+                    return [{
+                        status: item.state,
+                        start,
+                        end,
+                        class: item.state ?? 'active',
+                    }];
+                }
+
+                return item.state.map(state => ({
+                    status: state,
+                    start,
+                    end,
+                    class: state,
+                }));
+            });
+        }
+
+        private get statusHistoryWidth(): number {
+            const time = Date.now() - this.statusHistoryStartDate.getTime();
+            const ticks = this.statusHistoryTicks * 24 * 60 * 60 * 1000;
+
+            return time / ticks;
+        }
+
+        private get statusHistoryLabels(): string[] {
+            return [
+                'LOCA: active',
+                'LOCA: vacation',
+                'LOCA: inactive',
+                'LOCA: inactive-long',
+                'LOCA: banned',
+                'LOCA: outlaw',
+                'LOCA: deleted',
+                'LOCA: admin',
+            ];
+        }
+
+
         private async mounted() {
             await this.redirectToMeIfNoPlayersSelected();
 
@@ -346,5 +453,92 @@
         display: grid;
         grid-template-columns: 1fr auto 1fr;
         gap: 4px;
+    }
+
+    $status-bar-height: 40px;
+    $status-bar-margin: 5px;
+    $status: (
+        admin: #f48406,
+        banned: #ffffff,
+        vacation: #00ffff,
+        inactive: #6e6e6e,
+        inactive-long: #4f4f4f,
+        outlaw: #ff33ff,
+        active: #7aff43,
+        deleted: #880000,
+    );
+    $status-count: 8;
+    $status-offset: (
+        active: 0,
+        vacation: 1,
+        inactive: 2,
+        inactive-long: 3,
+        banned: 4,
+        outlaw: 5,
+        deleted: 6,
+        admin: 7,
+    );
+
+    .status-history-grid {
+        display: grid;
+        grid-template-columns: 100px 1fr;
+        grid-template-rows: #{$status-count * $status-bar-height + (
+                $status-count - 1
+            ) * $status-bar-margin} 100px;
+        column-gap: 16px;
+
+        > .graph,
+        > .x-ticks {
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            position: relative;
+        }
+
+        > .graph {
+            > .bar {
+                position: absolute;
+                height: $status-bar-height;
+                border-radius: 4px;
+
+                @each $name, $color in $status {
+                    &.#{$name} {
+                        background: $color;
+                        top: #{($status-bar-height + $status-bar-margin) *
+                            map-get($status-offset, $name)};
+                    }
+                }
+            }
+
+            > .y-tick {
+                height: $status-bar-height;
+                left: 0;
+                width: 100%;
+                top: calc(
+                    var(--y-tick) * #{$status-bar-height + $status-bar-margin}
+                );
+                position: absolute;
+
+                &::before {
+                    position: absolute;
+                    content: "";
+                    height: 1px;
+                    top: calc(50% - 0.5px);
+                    width: 100%;
+                    display: block;
+                    background: rgba(white, 0.1);
+                }
+            }
+        }
+
+        > .y-ticks {
+            width: 100%;
+            height: 100%;
+            grid-row: 1 / span 2;
+            display: grid;
+            grid-template-rows: repeat($status-count, $status-bar-height);
+            row-gap: $status-bar-margin;
+            align-items: center;
+        }
     }
 </style>
