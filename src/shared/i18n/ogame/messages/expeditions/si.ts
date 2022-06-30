00736b988c6459c2f68cecaea83e5a29f234c0e3
backfromexile <backfromexile@web.de>
2022-06-30 15:50:52 +0200

    bugfix si language found ships not tracking for expeditions

--- src/shared/i18n/ogame/messages/expeditions/si.ts
+++ src/shared/i18n/ogame/messages/expeditions/si.ts
@@ -54,7 +54,7 @@ export const si: ExpeditionMessages = {
             /*'Našli smo */'ogromno pokopališče ladij'/*. Našim tehnikom je uspelo nekatere popraviti.'*/,
             /*'Našli smo */'planet z ostanki civilizacije'/*. Opazili smo tudi ogromno vesoljsko postajo kako kroži v orbiti. Tehniki so se odpravili, da pogledajo katere ladje lahko uporabijo.'*/,
         ],
-        regex: (ships: string[]) => new RegExp(`Folgende Schiffe schlossen sich der Flotte an:(<br>|\\s*)(?<ships>((${ships.join('|')}):\\s*\\d+(<br>|\\s*)?)+)?`),
+        regex: (ships: string[]) => new RegExp(`Naslednje ladje so zdaj del tvoje flote:(<br>|\\s*)(?<ships>((${ships.join('|')}):\\s*\\d+(<br>|\\s*)?)+)?`),
     },
 
     [ExpeditionEventType.nothing]: [
