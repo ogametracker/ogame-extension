0f3efca23540a5d9b4e1bf99997d1f04208f1e61
backfromexile <backfromexile@web.de>
2022-07-12 13:35:33 +0200

    fix get active lifeform when no lifeform selected

--- src/content-scripts/empire-tracking/getActiveLifeform.ts
+++ src/content-scripts/empire-tracking/getActiveLifeform.ts
@@ -3,17 +3,23 @@ import { _throw } from "@/shared/utils/_throw";
 
 export function getActiveLifeform() {
     const element = document.querySelector('#lifeform') ?? _throw('did not find active lifeform element');
-    const classDiv = element.querySelector('div.lifeform-item-icon') ?? _throw('no lifeform icon found');
+    const classDiv = element.querySelector('div.lifeform-item-icon');
 
+    if(classDiv == null) {
+        return LifeformType.none;
+    }
     if (classDiv.classList.contains('lifeform1')) {
         return LifeformType.humans;
-    } else if (classDiv.classList.contains('lifeform2')) {
+    } 
+    else if (classDiv.classList.contains('lifeform2')) {
         return LifeformType.rocktal;
-    } else if (classDiv.classList.contains('lifeform3')) {
+    } 
+    else if (classDiv.classList.contains('lifeform3')) {
         return LifeformType.mechas;
-    } else if (classDiv.classList.contains('lifeform4')) {
+    } 
+    else if (classDiv.classList.contains('lifeform4')) {
         return LifeformType.kaelesh;
     }
 
-    return LifeformType.none;
+    throw new Error('found unknown lifeform icon');
 }
