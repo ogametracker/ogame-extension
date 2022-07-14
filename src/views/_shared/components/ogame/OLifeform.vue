eaee8183e641343ee22da3a0000f51367357cbbb
backfromexile <backfromexile@web.de>
2022-07-14 20:42:17 +0200

    added icons for all lifeform technologies and buildings, added shared components for lifeform stuff

---
+++ src/views/_shared/components/ogame/OLifeform.vue
@@ -0,0 +1,63 @@
+<template>
+    <div
+        class="o-lifeform"
+        :class="{
+            'o-lifeform--disabled': disabled,
+        }"
+        :style="{
+            'background-image': `url(/img/ogame/lifeforms/${image}.jpg)`,
+            'font-size': size,
+        }"
+        v-on="$listeners"
+        v-bind="{ ...$attrs, ...$props }"
+    />
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+    import { PropType } from 'vue';
+    import { LifeformType, LifeformTypes } from '@/shared/models/ogame/lifeforms/LifeformType';
+
+    @Component({})
+    export default class OLifeform extends Vue {
+        @Prop({
+            required: true,
+            type: String as PropType<LifeformType>,
+            validator: (value: string) => (LifeformTypes as string[]).includes(value)
+        })
+        private lifeform!: LifeformType;
+
+        @Prop({ required: false, type: String, default: '32px' })
+        private size!: string;
+
+        @Prop({ required: false, type: Boolean })
+        private disabled!: boolean;
+
+        private get image() {
+            return this.imageMap[this.lifeform];
+        }
+        private readonly imageMap: Record<LifeformType, string> = {
+            [LifeformType.humans]: 'humans',
+            [LifeformType.rocktal]: 'rocktal',
+            [LifeformType.mechas]: 'mechas',
+            [LifeformType.kaelesh]: 'kaelesh',
+            [LifeformType.none]: 'none',
+        };
+    }
+</script>
+<style lang="scss" scoped>
+    .o-lifeform {
+        width: 1em;
+        height: 1em;
+        display: inline-block;
+        background-size: cover;
+        background-position: center;
+        image-rendering: -webkit-optimize-contrast;
+        image-rendering: -moz-crisp-edges;
+        border-radius: 4px;
+
+        &--disabled {
+            filter: grayscale(1) brightness(0.7) contrast(1.2);
+        }
+    }
+</style>
---
+++ src/views/_shared/components/ogame/OLifeformBuilding.vue
@@ -0,0 +1,109 @@
+<template>
+    <div
+        class="o-lifeform-building"
+        :class="{
+            'o-lifeform-building--disabled': disabled,
+        }"
+        :style="{
+            'background-image': `url(/img/ogame/lifeforms/buildings/${image}.jpg)`,
+            'font-size': size,
+        }"
+        v-on="$listeners"
+        v-bind="{ ...$attrs, ...$props }"
+    />
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+    import { PropType } from 'vue';
+    import { LifeformBuildingType, LifeformBuildingTypes } from '@/shared/models/ogame/lifeforms/LifeformBuildingType';
+
+    @Component({})
+    export default class OLifeformBuilding extends Vue {
+        @Prop({
+            required: true,
+            type: Number as PropType<LifeformBuildingType>,
+            validator: (value: number) => (LifeformBuildingTypes as number[]).includes(value)
+        })
+        private building!: LifeformBuildingType;
+
+        @Prop({ required: false, type: String, default: '32px' })
+        private size!: string;
+
+        @Prop({ required: false, type: Boolean })
+        private disabled!: boolean;
+
+        private get image() {
+            return this.imageMap[this.building];
+        }
+        private readonly imageMap: Record<LifeformBuildingType, string> = {
+            [LifeformBuildingType.residentialSector]: 'residential-sector',
+            [LifeformBuildingType.biosphereFarm]: 'biosphere-farm',
+            [LifeformBuildingType.researchCentre]: 'research-centre',
+            [LifeformBuildingType.academyOfSciences]: 'academy-of-sciences',
+            [LifeformBuildingType.neuroCalibrationCentre]: 'neuro-calibration-centre',
+            [LifeformBuildingType.highEnergySmelting]: 'high-energy-smelting',
+            [LifeformBuildingType.foodSilo]: 'food-silo',
+            [LifeformBuildingType.fusionPoweredProduction]: 'fusion-powered-production',
+            [LifeformBuildingType.skyscraper]: 'skyscraper',
+            [LifeformBuildingType.biotechLab]: 'biotech-lab',
+            [LifeformBuildingType.metropolis]: 'metropolis',
+            [LifeformBuildingType.planetaryShield]: 'planetary-shield',
+
+            [LifeformBuildingType.meditationEnclave]: 'meditation-enclave',
+            [LifeformBuildingType.crystalFarm]: 'crystal-farm',
+            [LifeformBuildingType.runeTechnologium]: 'rune-technologium',
+            [LifeformBuildingType.runeForge]: 'rune-forge',
+            [LifeformBuildingType.oriktorium]: 'oriktorium',
+            [LifeformBuildingType.magmaForge]: 'magma-forge',
+            [LifeformBuildingType.disruptionChamber]: 'disruption-chamber',
+            [LifeformBuildingType.megalith]: 'megalith',
+            [LifeformBuildingType.crystalRefinery]: 'crystal-refinery',
+            [LifeformBuildingType.deuteriumSynthesiser]: 'deuterium-synthesiser',
+            [LifeformBuildingType.mineralResearchCentre]: 'mineral-research-centre',
+            [LifeformBuildingType.advancedRecyclingPlant]: 'advanced-recycling-plant',
+
+            [LifeformBuildingType.assemblyLine]: 'assembly-line',
+            [LifeformBuildingType.fusionCellFactory]: 'fusion-cell-factory',
+            [LifeformBuildingType.roboticsResearchCentre]: 'robotics-research-centre',
+            [LifeformBuildingType.updateNetwork]: 'update-network',
+            [LifeformBuildingType.quantumComputerCentre]: 'quantum-computer-centre',
+            [LifeformBuildingType.automatisedAssemblyCentre]: 'automatised-assembly-centre',
+            [LifeformBuildingType.highPerformanceTransformer]: 'high-performance-transformer',
+            [LifeformBuildingType.microchipAssemblyLine]: 'microchip-assembly-line',
+            [LifeformBuildingType.productionAssemblyHall]: 'production-assembly-hall',
+            [LifeformBuildingType.highPerformanceSynthesiser]: 'high-performance-synthesiser',
+            [LifeformBuildingType.chipMassProduction]: 'chip-mass-production',
+            [LifeformBuildingType.nanoRepairBots]: 'nano-repair-bots',
+
+            [LifeformBuildingType.sanctuary]: 'sanctuary',
+            [LifeformBuildingType.antimatterCondenser]: 'antimatter-condenser',
+            [LifeformBuildingType.vortexChamber]: 'vortex-chamber',
+            [LifeformBuildingType.hallsOfRealisation]: 'halls-of-realisation',
+            [LifeformBuildingType.forumOfTranscendence]: 'forum-of-transcendence',
+            [LifeformBuildingType.antimatterConvector]: 'antimatter-convector',
+            [LifeformBuildingType.cloningLaboratory]: 'cloning-laboratory',
+            [LifeformBuildingType.chrysalisAccelerator]: 'chrysalis-accelerator',
+            [LifeformBuildingType.bioModifier]: 'bio-modifier',
+            [LifeformBuildingType.psionicModulator]: 'psionic-modulator',
+            [LifeformBuildingType.shipManufacturingHall]: 'ship-manufacturing-hall',
+            [LifeformBuildingType.supraRefractor]: 'supra-refractor',
+        };
+    }
+</script>
+<style lang="scss" scoped>
+    .o-lifeform-building {
+        width: 1em;
+        height: 1em;
+        display: inline-block;
+        background-size: cover;
+        background-position: center;
+        image-rendering: -webkit-optimize-contrast;
+        image-rendering: -moz-crisp-edges;
+        border-radius: 4px;
+
+        &--disabled {
+            filter: grayscale(1) brightness(0.7) contrast(1.2);
+        }
+    }
+</style>
