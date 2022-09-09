03df27bfb9924b604bca4c59924c9f077095e1c1
backfromexile <backfromexile@web.de>
2022-09-09 15:59:44 +0200

    i18n bugfix (252)

--- src/shared/i18n/ogame/messages/debris-field-reports/en.ts
+++ src/shared/i18n/ogame/messages/debris-field-reports/en.ts
@@ -1,5 +1,5 @@
 import { DebrisFieldReportMessages } from "./types";
 
 export const en: DebrisFieldReportMessages = {
-    regex: /You have harvested (?<metal>[\d.]+) Metal and (?<crystal>[\d.]+) Crystal/,
+    regex: /You have harvested (?<metal>[\d,. ]+) Metal and (?<crystal>[\d,. ]+) Crystal/,
 };
