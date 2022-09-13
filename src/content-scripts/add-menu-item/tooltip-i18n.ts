99dd3309d1ce704c18b5f050c3e5e7ccee309e2f
backfromexile <backfromexile@web.de>
2022-09-13 18:59:48 +0200

    i18n refactor

--- src/content-scripts/add-menu-item/tooltip-i18n.ts
+++
@@ -1,5 +0,0 @@
-//TODO: move to i18n directory
-export const i18nTooltips: Partial<Record<string, string>> = {
-    de: 'Öffnet das Interface des OGame Trackers in einem neuen Tab.',
-    en: 'Opens the interface of the OGame Tracker in a new tab.',
-};
---
+++ src/shared/i18n/extension/ingame/en.ts
@@ -0,0 +1,5 @@
+import { ExtensionIngameTranslations } from "./type";
+
+export const en: ExtensionIngameTranslations = {
+    tooltipNewTab: 'Opens the interface of the OGame Tracker in a new tab.',
+};
