a9dbe8387a2a0b56ff0b4303f38adf359e80504f
backfromexile <backfromexile@web.de>
2022-10-12 17:39:38 +0200

    i18n: change locale names iwth underscore to contain a dash instead

--- src/shared/i18n/LanguageKey.ts
+++ src/shared/i18n/LanguageKey.ts
@@ -3,9 +3,9 @@ export enum LanguageKey {
     de = 'de', // german
     dk = 'dk', // danish
     en = 'en', // english
-    es_es = 'es_es', // spanish (Spain)
-    es_ar = 'es_ar', // spanish (Argentina)
-    es_mx = 'es_mx', // spanish (Mexico)
+    'es-es' = 'es-es', // spanish (Spain)
+    'es-ar' = 'es-ar', // spanish (Argentina)
+    'es-mx' = 'es-mx', // spanish (Mexico)
     fr = 'fr', // french
     //TODO: gr = 'gr', // greek
     hr = 'hr', // croatian
@@ -14,12 +14,12 @@ export enum LanguageKey {
     //TODO: jp = 'jp', // japanese
     //TODO: nl = 'nl', // dutch
     pl = 'pl', // polish
-    pt_br = 'pt_br', // portuguese (Brasil)
-    pt_pt = 'pt_pt', // portuguese (Portugal)
+    'pt-br' = 'pt-br', // portuguese (Brasil)
+    'pt-pt' = 'pt-pt', // portuguese (Portugal)
     //TODO: ro = 'ro', // romanian
     //TODO: ru = 'ru', // russian
     si = 'si', // slovenian
     //TODO: sk = 'sk', // slovakian
     //TODO: tr = 'tr', // turkish
-    zh_tw = 'zh_tw', // chinese (Taiwan)
+    'zh-tw' = 'zh-tw', // chinese (Taiwan)
 }
