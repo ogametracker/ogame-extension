637a22f2df6120edcaeb21a7082addf5ce1a71f2
backfromexile <backfromexile@web.de>
2023-03-07 12:55:20 +0100

    i18n: added some missing GR messages

--- src/shared/i18n/ogame/messages/expeditions/el.ts
+++ src/shared/i18n/ogame/messages/expeditions/el.ts
@@ -6,9 +6,8 @@ import { ExpeditionMessages } from "./types";
 export const el: ExpeditionMessages = {
     [ExpeditionEventType.darkMatter]: {
         [ExpeditionEventSize.small]: [
-            /*'Η αποστολή */'εξερεύνησης δεν μπόρεσε να εντοπίσει αντιύλη', //TODO: ist das die Nachricht darunter?
-            /*TODO: el 'Der Expedition ist es gelungen, */'ein wenig Dunkle Materie einzufangen'/* und zu konservieren.'*/,
-            /*TODO: el 'Wir haben die */'Überreste eines Alien-Schiffes'/* gefunden. An Bord war ein kleiner Behälter mit Dunkler Materie!'*/,
+            /*'Η αποστολή */'εξερεύνησης δεν μπόρεσε να εντοπίσει αντιύλη',
+            /*'Εντοπίστηκαν τα */'απομεινάρια ενός εξωγήινου σκάφους'/* . Στο κύτος του βρέθηκε ένα κιβώτιο με μικρή ποσότητα αντιύλης!'*/,
             /*'Συναντήσαμε εν παράξενο ον, */'που μας πρόσφερε ένα κιβώτιο με αντιύλη ως αντάλαγμα'/* , για μερικούς απλούς μαθηματικούς υπολογισμούς'*/,
             /*'Η αποστολή */'εξερεύνησης κατέλαβε ένα σκάφος-φάντασμα'/* . Δε στάθηκε δυνατό μάθουμε τι απέγινε το πλήρωμα αλλά οι τεχνικοί μπορέσαν και συγκέντρωσαν την αντιύλη που μετέφερε.'*/,
             /*'Η εξερευνητική αποστολή ακολούθησε κάποιες */'παράξενες ενδείξεις που προέρχοταν από ένα αστεροϊδή'/* . Στο πυρήνα του, εντοπίστηκε αντιύλη. Η ερευνητές πήραν τον αστεροϊδή για να προσπαθήσουν να εξορύξουν την αντιύλη.'*/,
@@ -27,11 +26,11 @@ export const el: ExpeditionMessages = {
 
     [ExpeditionEventType.resources]: {
         [ExpeditionEventSize.small]: [
+            /*'Η αποστολή */'εξερεύνησης εντόπισε ένα μικρό αστεροϊδή'/* , από τον οποίο μπορούν να εξορυχθούν κάποιοι πόροι.'*/,
             /*'Σε ένα απομονωμένο */'αστεροϊδή ανακαλύψαμε κάποιους εύκολα προσβάσιμα πεδία'/* πόρων και εξορύξαμε με επιτυχία μερικούς.'*/,
-            /*TODO: el 'Auf einem abgelegenen Planetoiden wurden */'einige leicht zugängliche Ressourcenfelder'/* gefunden und erfolgreich Rohstoffe gewonnen'*/,
         ],
         [ExpeditionEventSize.medium]: [
-            /*TODO: el 'Deine Expedition fand einen uralten, voll beladenen, aber */'menschenleeren Frachterkonvoi'/*. Einige Ressourcen konnten geborgen werden.'*/,
+            /*'Η αποστολή εξερεύνησης */'εντόπισε ένα αρχαίο κομβόι μεταγωγικών με γεμάτα αμπάρια'/* . Μερικοί από τους πόρους μπορούν να χρησιμοποιηθούν.'*/,
             /*'Σε ένα */'μικρό φεγγάρι με δική του ατμόσφαιρα'/* , η αποστολή εντόπισε τεράστιες ποσότητες ακατέργαστων πόρων. Το πλήρωμα βρίσκεται στο έδαφος και προσπαθεί να φορτώσει αυτό το φυσικό θυσαυρό.'*/,
         ],
         [ExpeditionEventSize.large]: [
@@ -62,10 +61,10 @@ export const el: ExpeditionMessages = {
     [ExpeditionEventType.nothing]: [
         /*'Δυστυχώς, παρά τις πολλά */'υποσχόμενες αρχικές ενδείξεις στο τομέα'/*, επιστρέψαμε πίσω με άδεια χέρια...'*/,
         /*'Εκτός από μερικά περίεργα, */'μικρά ζώα από έναν άγνωστο ελώδη πλανήτη'/* , αυτή η αποστολή δεν φέρνει τίποτα συγκλονιστικό επιστρέφοντας από το ταξίδι.'*/,
-        /*TODO: el 'Deine Expedition hat wortwörtlich mit der */'Leere des Alls'/* Bekanntschaft gemacht. Es gab nicht einmal einen kleinen Asteroiden oder Strahlung oder Partikel oder irgendetwas, das diese Expedition aufregend gestaltet hätte.'*/,
+        /*'Η αποστολή έμαθε */'για το αχανές κενό του διαστήματος'/* . Δεν υπήρχε ούτε καν ένας μικρός αστεροειδής ή ακτινοβολία ή σωματίδιο που θα έκανε αυτή την αποστολή ενδιαφέρουσα.'*/,
         /*'Μια οντότητα, */'αποτελούμενη από καθαρή ενέργεια'/* , φρόντισε ώστε τα πληρώματα της αποστολής να αποροφηθούν κοιτάζοντας σαν υπνωτισμένοι τα σχέδια που δημιουργούσε στις οθόνες. Όταν οι περισσότεροι συνήλθαν, έπρεπε να ματαιώσουν την αποστολή καθώς είχε απομείνει ελάχιστο Δευτέριο.'*/,
         /*'Μια αποτυχία στον */'αντιδραστήρα του ηγούμενου σκάφους της αποστολής'/* , παραλίγο να καταστρέψει όλο το στόλο. Ευτυχώς οι μηχανικοί ήταν κάτι παραπάνω από ικανοί και μπόρεσαν να αποφύγουν τα χειρότερα. Οι επισκευές απαίτησαν αρκετό χρόνο και υποχρέωσαν την αποστολή να επιστρέψει χωρίς να έχει επιτύχει τίποτα.'*/,
-        /*TODO: el 'Deine Expedition hat */'wunderschöne Bilder einer Supernova'/* gemacht. Wirklich neue Erkenntnisse hat diese Expedition jedoch nicht gebracht. Aber man hat gute Chancen auf den Sieg im diesjährigen Bestes-Bild-des-Universums-Wettbewerb!'*/,
+        /*'Η αποστολή έβγαλε μερικές */'φαντασμαγορικές φωτογραφίες από ένα σούπερ Νόβα'/* . Τίποτα άλλο δεν επιτεύχθηκε σε αυτή την αποστολή, αλλά τουλάχιστον υπάρχουν πιθανότητες να κερδίσουμε στο φετινό διαγωνισμό "Καλύτερη φωτογραφία του Σύμπαντος"'*/,
         /*'Η αποστολή σας ακολούθησε περίεργα σήματα για κάποιο διάστημα. Στο τέλος */'διαπίστωσαν ότι τα σήματα εκπέμπονταν από ένα παλιό κατασκοπευτικό'/* που είχε αποσταλεί γενεές πριν να συναντήσει ξένα είδη. Το κατασκοπευτικό διασώθηκε και κάποια μουσεία από τον κεντρικό σας πλανήτη, έχουν ήδη εκφράσει το ενδιαφέρον τους.'*/,
         /*'Τώρα λοιπόν γνωρίζουμε, πως αυτές οι */'αστρικές διαταραχές επιπέδου 5'/* , εκτός από τα χαοτικά αποτελέσματα που έχουν στα συστήματα πλοήγησης των σκαφών, δημιουργούν και φαινόμαινα μαζικών παραισθήσεων στο πλήρωμα. Η αποστολή επέστρεψε χωρίς να φέρει τίποτα πίσω.'*/,
         /*'Η αποστολή σας, παραλίγο να */'εγκλωβιστεί στο πεδίο βαρύτητας ενός ουδέτερου πλανήτη και χρειάστηκε'/* αρκετός χρόνος για να διαφύγει. Εξαιτίας του γεγονότος αυτού, καταναλώθηκε μεγάλη ποσότητα Δευτερίου και ο στόλος υποχρεώθηκε να επιστρέψει άπρακτος.'*/,
@@ -76,29 +75,29 @@ export const el: ExpeditionMessages = {
 
     [ExpeditionEventType.lostFleet]: [
         /*TODO: el 'Das Letzte, was von dieser Expedition noch gesendet wurde, waren einige unglaublich gut gelungene */'Nahaufnahmen eines sich öffnenden schwarzen Lochs'/*.'*/,
-        /*TODO: el 'Von der Expedition ist */'nur noch folgender Funkspruch übrig'/* geblieben: Zzzrrt Oh Gott! Krrrzzzzt dass zrrrtrzt sieht krgzzzz ja aus wie Krzzzzzzzztzzzz ...'*/,
-        /*TODO: el 'Ein Kernbruch des Führungsschiffes führte zu einer Kettenreaktion, die in einer durchaus */'spektakulären Explosion die gesamte Expedition'/* vernichtete.'*/,
-        /*TODO: el 'Die Expeditionsflotte ist */'nicht mehr aus dem Sprung in den Normalraum'/* zurückgekehrt. Unsere Wissenschaftler rätseln noch immer, was geschehen sein könnte, jedoch scheint die Flotte endgültig verloren zu sein.'*/,
+        /*'Το μόνο που απέμεινε από την αποστολή εξερεύνησης, */'είναι αυτή η τελευταία επικοινωνία'/* : zzzzzz....αυτό....zzzzzz....τρομερό....zzzzzz....δεν....zzzzzz....βοήθ....zzzzzzzzzzzzzzzzz... Τέλος μετάδοσης'*/,
+        /*'Ένα πρόβλημα στο σύστημα ψύξης του αντιδραστήρα, στο προπορευόμενο σκάφος, είχε σαν αποτέλεσμα μια αλυσιδωτή αντίδραση που με μιά */'θεαματική πυρηνική έκρηξη κατάστρεψε όλο το στόλο'/* που ακολουθούσε.'*/,
+        /*'Ο στόλος */'εξερεύνησης δεν επέστρεψε ποτέ...'/* Οι επιστήμονες προσπαθούν να ανακαλύψουν τι συνέβη, αλλά φαίνεται ότι ο στόλος χάθηκε για πάντα...'*/,
     ],
 
     [ExpeditionEventType.trader]: [
         /*'Ο στόλος της */'αποστολής σας ήρθε σε επαφή με μια ντροπαλή εξωγήινη'/* φυλή. Ανακοίνωσαν ότι θα στείλουν στους κόσμους σας, αντιπρόσωπους με αγαθά προς ανταλλαγή.'*/,
-        /*TODO: el 'Deine Expeditionsflotte hatte ein Notsignal aufgefangen. Es handelte sich um einen Megafrachter, der im starken Gravitationsfeld eines Planetoiden gefangen war. Nachdem der Frachter erfolgreich befreit worden war, verkündete der Frachterkapitän feierlich, seine Befreier als bevorzugte Exklusivkunden */'in sein schwarzes Buch'/* aufzunehmen.'*/,
+        /*'Η */'αποστολή σας έλαβε σήμα κινδύνου'/* . Ένα μεγάλο μεταγωγικό πιάστηκε στο ισχυρό βαρυτικό πεδίο ενός πλανητοειδούς. Μετά την επιτυχή διάσωση του μεταγωγικού, ο πλοίαρχος ανακοίνωσε χαρούμενα ότι το άτομο που τον έσωσε γίνεται ο αγαπημένος και αποκλειστικός του πελάτης.'*/,
     ],
 
     [ExpeditionEventType.early]: [
         /*'Μια απρόσμενη */'ανάστροφη σύζευξη στα ενεργειακά στροφεία επίσπευσε την επιστροφή'/* της αποστολής, επιστρέφει νωρίτερα από το αναμενόμενο. Οι πρώτες αναφορές ενημερώνουν ότι δεν υπάρχει κάτι αξιόλογο να αναφέρουν.'*/,
-        /*TODO: el 'Deine Expedition meldet keine Besonderheiten in dem erforschten Sektor. Jedoch geriet die Flotte */'beim Rücksprung in einen Sonnenwind'/*. Dadurch wurde der Sprung beschleunigt. Deine Expedition kehrt nun etwas früher nach Hause.'*/,
-        /*TODO: el 'Der etwas wagemutige neue */'Kommandant nutzte ein instabiles Wurmloch'/*, um den Rückflug zu verkürzen - mit Erfolg! Jedoch hat die Expedition selbst keine neuen Erkenntnisse gebracht.'*/,
+        /*'Η αποστολή σας δεν αναφέρει ανωμαλίες στον υπό αναγνώριση τομέα. Αλλά */'ο στόλος βρέθηκε σε ηλιακή θύελλα ενώ επέστρεφε'/* . Για αυτό το λόγο το ταξίδι της επιστροφής συντόμευσε κατά πολύ. Η αποστολή σας θα γυρίσει νωρίτερα.'*/,
+        /*'Ο νέος και τολμηρός διοικητής, */'ταξίδευσε με επιτυχία μέσω μιας ασταθούς σκουληκότρυπας'/* για να συντομεύσει την επιστροφή! Παρ’όλα αυτά η αποστολή δεν έφερε κάτι καινούριο.'*/,
     ],
 
     [ExpeditionEventType.delay]: [
-        /*TODO: el 'Ein böser Patzer des Navigators führte zu einer */'Fehlkalkulation beim Sprung'/* der Expedition.'*/,
+        /*'Ο κυβερνήτης της αποστολής δε κοιμήθηκε καλά το βράδι, */'με αποτέλεσμα κάνει λάθος υπολογισμούς'/* για το πέρασμα από τη Διαγαλαξιακή Πύλη. Όχι μόνο μετέβη ο στόλος σε τελείως διαφορετικό σημείο από το προκαθορισμένο, αλλά θα χρειαστεί και πολύ περισσότερο χρόνο για να επιστρέψει.'*/,
         /*'Η αποστολή σας */'βρέθηκε σε τομέα σωματιδιακών καταιγίδων'/* . Με αποτέλεσμα να υπερφορτωθούν οι αποθήκες ενέργειας και να καταστραφούν τα περισσότερα κύρια συστήματα των σκαφών. Οι μηχανικοί σας απόφυγαν τα χειρότερα αλλά η αποστολή θα επιστρέψει με μεγάλη καθυστέρηση'*/,
         /*'Για άγνωστη αιτία, το άλμα της αποστολής πήγε στραβά. Σχεδόν */'προσγειώθηκε στην καρδιά ενός ήλιου'/* . Ευτυχώς προσγειώθηκε σε γνωστό σύστημα, αλλά το άλμα επιστροφής θα καθυστερήσει περισσότερο από το αναμενόμενο.'*/,
         /*'Ο */'αστρικός άνεμος ενός κόκκινου γίγαντα κατάστρεψε'/* το άλμα της αποστολής, θα πάρει αρκετή ώρα να υπολογιστεί το άλμα επιστροφής. Μεταξύ των άστρων του τομέα, δεν υπήρχε παρά το κενό. Ο στόλος θα επιστρέψει αργότερα από το αναμενόμενο.'*/,
-        /*TODO: el 'Das neue */'Navigationsmodul hat wohl doch noch mit einigen Bugs'/* zu kämpfen. Nicht nur ging der Sprung der Expeditionsflotte in die völlig falsche Richtung, auch wurde das gesamte Deuterium verbraucht, wobei der Sprung der Flotte nur knapp hinter dem Mond des Startplaneten endete. Etwas enttäuscht kehrt die Expedition nun auf Impuls zurück. Dadurch wird die Rückkehr wohl ein wenig verzögert.'*/,
-        /*TODO: el 'Das Führungsschiff deiner */'Expeditionsflotte kollidierte mit einem fremden Schiff'/*, das ohne Vorwarnung direkt in die Flotte sprang. Das fremde Schiff explodierte und die Schäden am Führungsschiff waren beachtlich. Sobald die gröbsten Reparaturen abgeschlossen sind, werden sich deine Schiffe auf den Rückweg machen, da in diesem Zustand die Expedition nicht fortgeführt werden kann.'*/,
+        /*'Το */'νέο σύστημα ναυσιπλοΐας αντιμετωπίζει προβλήματα'/* . Το άλμα αποστολής όχι μόνο τους οδήγησε σε εντελώς λανθασμένη κατεύθυνση αλλά χρησιμοποιήθηκε και όλο το δευτέριο. Ευτυχώς, το άλμα του στόλου κατέληξε κοντά στο φεγγάρι του πλανήτη εκκίνησης. Απογοητευμένη η αποστολή, επιστρέφει χωρίς ώθηση. Το ταξίδι της επιστροφής θα καθυστερήσει λίγο παραπάνω.'*/,
+        /*'Η */'ναυαρχίδα της αποστολής συγκρούστηκε με ξένο σκάφος'/* που έκανε άλμα χωρίς προειδοποίηση. Το ξένο σκάφος ανατινάχτηκε και η ζημιά στη ναυαρχίδα ήταν εκτεταμένη. Μόλις ολοκληρωθούν οι απαραίτητες επισκευές, ο στόλος θα ξεκινήσει το ταξίδι της επιστροφής καθώς η αποστολή δε μπορεί να συνεχιστεί κάτω από αυτές τις συνθήκες.'*/,
     ],
 
     [ExpeditionEventType.item]: {
@@ -109,15 +108,15 @@ export const el: ExpeditionMessages = {
         [ExpeditionEventSize.small]: [
             /*'Πέσαμε σε */'ενέδρα που είχε στηθεί από τους αστροπειρατές'/* . Η μάχη ήταν αναπόφευκτη.'*/,//TODO: welche Nachricht ist das?
             /*'Μερικοί */'πραγματικά απελπισμένοι πειρατές του διαστήματος'/* προσπάθησαν να καταλάβουν την εξερευνητική αποστολή.'*/,
-            /*TODO: el 'Einige */'primitive Barbaren greifen uns mit Raumschiffen'/* an, die nicht einmal ansatzweise die Bezeichnung Raumschiff verdient haben. Sollte der Beschuss ernstzunehmende Ausmaße annehmen, sehen wir uns gezwungen, das Feuer zu erwidern.'*/,
-            /*TODO: el 'Wir haben ein paar */'Funksprüche sehr betrunkener Piraten'/* aufgefangen.'*/,
-            /*TODO: el 'Wir */'mussten uns gegen einige Piraten wehren'/*, die zum Glück nicht allzu zahlreich waren.'*/,
+            /*'Μία */'πρωτόγονη φυλή πειρατών μας επιτίθεται'/* με τα αστροσκάφη τους, αν μπορούμε να τα ονομάσουμε έτσι. Αν τα πυρά τους γίνουν επικύνδινα θα αναγκαστούμε να ανταποδώσουμε.'*/,
+            /*'Υποκλέψαμε */'κάποια ραδιοσήματα από κάποιους μεθυσμένους αστροπειρατές'/* . Μάλλον θα μας επιτεθούν σύντομα.'*/,
+            /*'Έπρεπε να */'αντιμετωπίσουμε μερικούς πειρατές'/*. Ευτυχώς δεν ήταν πολλοί.'*/,
             /*TODO: el 'Unsere Expeditionsflotte meldet, dass ein gewisser */'Moa Tikarr und seine wilde Meute'/* die bedingungslose Kapitulation unserer Flotte verlangen. Sollten sie Ernst machen, werden sie feststellen müssen, dass sich unsere Schiffe durchaus zu wehren wissen.'*/,
         ],
         [ExpeditionEventSize.medium]: [
             /*TODO: el 'Deine Expeditionsflotte hatte ein */'unschönes Zusammentreffen mit einigen Weltraumpiraten'/*.'*/,
             /*TODO: el 'Wir sind in den */'Hinterhalt einiger Sternen-Freibeuter'/* geraten!'*/,
-            /*TODO: el 'Der Hilferuf, dem die Expedition folgte, stellte sich als */'böse Falle einiger arglistiger Sternen-Freibeuter'/* heraus. Ein Gefecht war unvermeidlich.'*/,
+            /*'Το σήμα κινδύνου που ακολουθήσαμε, ήταν δόλωμα για την */'ενέδρα που είχαν στήσει οι αστροπειρατές'/* . Η μάχη ήταν αναπόφευκτη.'*/,
         ],
         [ExpeditionEventSize.large]: [
             /*TODO: el 'Die aufgefangenen Signale stammten nicht von Fremdwesen, sondern */'von einer geheimen Piratenbasis'/* ! Die Piraten waren von unserer Anwesenheit in ihrem Sektor nicht besonders begeistert.'*/,
@@ -127,14 +126,14 @@ export const el: ExpeditionMessages = {
 
     [ExpeditionEventType.aliens]: {
         [ExpeditionEventSize.small]: [
-            /*TODO: el 'Einige */'fremdartig anmutende Schiffe'/* haben ohne Vorwarnung die Expeditionsflotte angegriffen.'*/,
+            /*'Μερικά σκάφη με εντυπωσιακή εμφάνιση, */'επιτέθηκαν στο εξερευνητικό στόλο απροειδοποίητα',
             /*'Ο εξερευνητικός στόλος σας, */'ήρθε σε όχι και τόσο φιλική επαφή'/* , με κάποια άγνωστης προέλευσης όντα.'*/,
-            /*TODO: el 'Unsere Expedition wurde von einer */'kleinen Gruppe unbekannter Schiffe'/* angegriffen.'*/,
+            /*'Η αποστολή */'δέχτηκε επίθεση από ένα μικρό σχηματισμό άγνωστων'/* σκαφών!'*/,
             /*TODO: el 'Die Expeditionsflotte meldet */'Kontakt mit unbekannten Schiffen'/*. Die Funksprüche sind nicht entschlüsselbar, jedoch scheinen die fremden Schiffe ihre Waffen zu aktivieren.'*/,
         ],
         [ExpeditionEventSize.medium]: [
             /*TODO: el 'Eine */'unbekannte Spezies greift unsere Expedition'/* an!'*/,
-            /*TODO: el 'Deine */'Expeditionsflotte hat anscheinend das Hoheitsgebiet'/* einer bisher unbekannten, aber äußerst aggressiven und kriegerischen Alienrasse verletzt.'*/,
+            /*'Ο στόλος της αποστολής, */'εισχώρησε σε μια περιοχή που ανήκει σε μια μη καταγεγραμένη'/* , αλλά πολύ επιθετική και πολεμοχαρή εξωγήινη φυλή.'*/,
             /*TODO: el 'Die Verbindung zu unserer Expeditionsflotte wurde kurzfristig gestört. Sofern wir die letzte Botschaft richtig entschlüsselt haben, steht die Flotte unter schwerem Feuer; die */'Aggressoren konnten nicht identifiziert werden'/*.'*/,
         ],
         [ExpeditionEventSize.large]: [
