b09f9903d535f7660b87c13fa9109adfcc027a8b
backfromexile <backfromexile@web.de>
2023-03-30 20:36:02 +0200

    i18n: added CZ debris field message with deuterium

--- src/shared/i18n/ogame/messages/debris-field-reports/cz.ts
+++ src/shared/i18n/ogame/messages/debris-field-reports/cz.ts
@@ -3,6 +3,6 @@ import { DebrisFieldReportMessages } from "./types";
 export const cz: DebrisFieldReportMessages = {
     regex: [
         /Vytěžil jsi (?<metal>.+) kovu a (?<crystal>.+) krystalů/i,
-        /Vytěžil jsi (?<metal>.+), (?<crystal>.+) krystalů kovu a (?<deuterium>.+) deuterium/i,
+        /Získáváš (?<metal>.+) kovu, (?<crystal>.+) (?<deuterium>.+) a 0 deuteria./i,
     ],
 };
