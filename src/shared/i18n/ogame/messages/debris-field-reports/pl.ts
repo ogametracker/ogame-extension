68c7b093d94c08295b72b2ee8137cf69cb021776
backfromexile <backfromexile@web.de>
2023-03-30 17:43:33 +0200

    i18n: added PL debris field message with deuterium

--- src/shared/i18n/ogame/messages/debris-field-reports/pl.ts
+++ src/shared/i18n/ogame/messages/debris-field-reports/pl.ts
@@ -3,5 +3,6 @@ import { DebrisFieldReportMessages } from "./types";
 export const pl: DebrisFieldReportMessages = {
     regex: [
         /Udało ci się odzyskać (?<metal>.+) szt. metalu i (?<crystal>.+) szt. kryształu/i,
+        /Udało ci się odzyskać (?<metal>.+) szt. metalu i (?<crystal>.+) szt. kryształu oraz (?<deuterium>.+) deuteru./i
     ],
 };
