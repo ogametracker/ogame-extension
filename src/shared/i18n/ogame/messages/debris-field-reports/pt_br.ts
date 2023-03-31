96ae651a86504aefbeac9818f77c4e46d4087e8a
backfromexile <backfromexile@web.de>
2023-03-31 12:40:18 +0200

    i18n: added PT-BR and PT debris field message with deuterium

--- src/shared/i18n/ogame/messages/debris-field-reports/pt_br.ts
+++ src/shared/i18n/ogame/messages/debris-field-reports/pt_br.ts
@@ -3,6 +3,6 @@ import { DebrisFieldReportMessages } from "./types";
 export const pt_br: DebrisFieldReportMessages = {
     regex: [
         /Recolheste (?<metal>.+) Metal e (?<crystal>.+) Cristal/i,
-        /Recolheste (?<metal>.+) Metal, (?<crystal>.+) Cristal e (?<deuterium>.+) deutério/i,
+        /Recolheste (?<metal>.+) Metal, (?<crystal>.+) Cristal e (?<deuterium>.+) Deutério/i,
     ],
 };
