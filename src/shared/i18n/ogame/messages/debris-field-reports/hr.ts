2e4c386e200c021ebd2e561baadf943f02bd6071
backfromexile <backfromexile@web.de>
2023-05-02 08:43:30 +0200

    i18n: added some missing HR messages

--- src/shared/i18n/ogame/messages/debris-field-reports/hr.ts
+++ src/shared/i18n/ogame/messages/debris-field-reports/hr.ts
@@ -2,6 +2,7 @@ import { DebrisFieldReportMessages } from "./types";
 
 export const hr: DebrisFieldReportMessages = {
     regex: [
-        /Ti izvlacis (?<metal>.+) Metala i (?<crystal>.+) Kristala./i,
+        /Ti izvlacis (?<metal>.+) Metala i (?<crystal>.+) Kristala/i,
+        /Izvukli ste (?<metal>.+) Metala, (?<crystal>.+) Kristala i (?<deuterium>.+) Deuterija/i,
     ],
 };
