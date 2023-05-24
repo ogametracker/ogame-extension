eb479837f522d70f40a11e283148af6efb711a47
backfromexile <backfromexile@web.de>
2023-05-24 23:03:45 +0200

    i18n: added some missing NL messages

--- src/shared/i18n/ogame/messages/debris-field-reports/nl.ts
+++ src/shared/i18n/ogame/messages/debris-field-reports/nl.ts
@@ -3,6 +3,6 @@ import { DebrisFieldReportMessages } from "./types";
 export const nl: DebrisFieldReportMessages = {
     regex: [
         /Je hebt (?<metal>.+) metaal en (?<crystal>.+) kristal opgehaald/i,
-        /Je hebt (?<metal>.+) metaal, (?<crystal>.+) kristal en (?<deuterium>.+) deuterium opgehaald/i,
+        /Je hebt (?<metal>.+) metaal, (?<crystal>.+) kristal en (?<deuterium>.+) deuterium verzameld/i
     ]
 };
