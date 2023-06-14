b8c2cebc3ef10446c6308ceaf084f4b8cd6572e8
backfromexile <backfromexile@web.de>
2023-06-14 22:10:13 +0200

    i18n: fix tracking for FR servers so it works with v9 and v10

--- src/shared/i18n/ogame/messages/message-titles/fr.ts
+++ src/shared/i18n/ogame/messages/message-titles/fr.ts
@@ -1,7 +1,7 @@
 import { MessageTitles } from "./types";
 
 export const fr: MessageTitles = {
-    expedition: 'Résultat de l\'expédition',
+    expedition: /*'Résultat de l\'*/'expédition',
     lifeformDiscovery: 'Rapport de forme de vie',
-    debrisField: 'Rapport d\'exploitation du champ de débris',
+    debrisField: /*'Rapport d\'*/'exploitation du champ de débris',
 };
