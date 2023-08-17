96e5fa0df3ff2ec956a33a82ec762e8ee3e3b421
backfromexile <backfromexile@web.de>
2023-08-17 21:28:12 +0200

    added RO support

---
+++ src/shared/i18n/ogame/resources/ro.ts
@@ -0,0 +1,9 @@
+import { ResourceType } from "../../../models/ogame/resources/ResourceType";
+import { ResourceTranslations } from "./types";
+
+export const ro: ResourceTranslations = {
+    [ResourceType.metal]: 'Metal',
+    [ResourceType.crystal]: 'Cristal',
+    [ResourceType.deuterium]: 'Deuteriu',
+    energy: 'Energie',
+};
