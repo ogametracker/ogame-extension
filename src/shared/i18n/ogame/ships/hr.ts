ad12337fce96dc1cd041e4769f29f41ce9e8541b
backfromexile <backfromexile@web.de>
2023-09-06 23:05:02 +0200

    added some missing HR messages, fix HR ship name

--- src/shared/i18n/ogame/ships/hr.ts
+++ src/shared/i18n/ogame/ships/hr.ts
@@ -4,11 +4,11 @@ import { ShipTranslations } from "./types";
 export const hr: ShipTranslations =  {
     [ShipType.lightFighter]: 'Mali lovac',
     [ShipType.heavyFighter]: 'Veliki lovac',
-    [ShipType.cruiser]: 'Krstarice',
-    [ShipType.battleship]: 'Borbeni brodovi',
+    [ShipType.cruiser]: 'Krstarica',
+    [ShipType.battleship]: 'Borbeni brod',
     [ShipType.battlecruiser]: 'Oklopna krstarica',
     [ShipType.bomber]: 'Bombarder',
-    [ShipType.destroyer]: 'Razaraci',
+    [ShipType.destroyer]: 'Razarač',
     [ShipType.deathStar]: 'Zvijezda smrti',
     [ShipType.reaper]: 'Žetelac',
     [ShipType.pathfinder]: 'Krčilac',
@@ -16,7 +16,24 @@ export const hr: ShipTranslations =  {
     [ShipType.largeCargo]: 'Veliki transporter',
     [ShipType.recycler]: 'Recikler',
     [ShipType.colonyShip]: 'Kolonijalni brod',
-    [ShipType.espionageProbe]: 'Sonde za spijunazu',
+    [ShipType.espionageProbe]: 'Sonde za špijunažu',
     [ShipType.crawler]: 'Puzavac',
     [ShipType.solarSatellite]: 'Solarni satelit',
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
