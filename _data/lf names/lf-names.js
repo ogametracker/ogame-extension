f7a1a0eaaca7b64bf7ac1bc7053ff7b713c13d88
backfromexile <backfromexile@web.de>
2023-09-07 13:27:53 +0200

    prepare for switching repository visibibilty to public

--- _data/lf names/lf-names.js
+++
@@ -1,3 +0,0 @@
-console.log( Array.from(document.querySelectorAll(`[data-category^="buildingLifeform"] .technologyRow .technologyInfo .technologyName`)).map(e => e.textContent.trim()).join('\n'));
-
-console.log( Array.from(document.querySelectorAll(`[data-category^="researchLifeform"] .technologyRow .technologyInfo .technologyName`)).map(e => e.textContent.trim()).join('\n'));
