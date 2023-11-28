948c91b74a63a3c6191670a11aa07b388cde649c
Rodrigo Correia <rodrigo@correia.red>
2023-11-28 14:46:46 +0000

    Translate - Common - PT-PT

---
+++ src/shared/i18n/extension/common/pt-pt.ts
@@ -0,0 +1,17 @@
+import { CommonTranslations } from "./type";
+
+export const en: CommonTranslations = {
+    averagePerDay: '⌀ por dia',
+    chart: 'Gráfico',
+    table: 'Tabela',
+    sum: 'Soma',
+    total: 'total',
+    resourceUnits: 'Unidades de Recursos',
+    dsu: 'DSU',
+    msu: 'MSU',
+    date: 'Data',
+    dateTime: 'Data & Hora',
+
+    madeWithLove1: 'Feito com',
+    madeWithLove2: 'por Wonkydonky',
+};
