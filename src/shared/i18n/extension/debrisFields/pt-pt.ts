583e96094011817c936a28b3856640be9e1f7d06
Rodrigo Correia <rodrigo@correia.red>
2023-11-28 14:47:48 +0000

    Translation - DebrisFields - PT-PT

---
+++ src/shared/i18n/extension/debrisFields/pt-pt.ts
@@ -0,0 +1,10 @@
+import { DebrisFieldsTranslations } from "./type";
+
+export const en: DebrisFieldsTranslations = {
+    header: 'Campo de Destroços',
+    addDebrisFieldManually: {
+        title: 'Adicionar campo de destroços manualmente',
+        buttonText: 'Adicionar',
+    },
+    position: 'Posição',
+};
