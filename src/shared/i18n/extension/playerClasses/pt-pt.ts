7713013c6d8087d717316315095fec92ee7bbac4
Rodrigo Correia <rodrigo@correia.red>
2023-11-28 14:54:56 +0000

    Translation - PlayerClasses - PT-PT

---
+++ src/shared/i18n/extension/playerClasses/pt-pt.ts
@@ -0,0 +1,8 @@
+import { PlayerClassTranslations } from "./type";
+
+export const en: PlayerClassTranslations = {
+    collector: 'Colecionador',
+    discoverer: 'Descobridor',
+    general: 'General',
+    none: 'Nenhuma',
+};
