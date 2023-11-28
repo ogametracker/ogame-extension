0ed7c3bd0b32a5827c08b5bdc11249a2d514aa9b
Rodrigo Correia <rodrigo@correia.red>
2023-11-28 14:49:56 +0000

    Translation - Donate - PT-PT

---
+++ src/shared/i18n/extension/donate/pt-pt.ts
@@ -0,0 +1,15 @@
+import { DonateTranslations } from "./type";
+
+export const en: DonateTranslations = {
+    header: 'Doação',
+    subHeader: 'Doar para ajudar o desenvolvimento do OGame Tracker',
+    
+    crypto: {
+        header: 'Doar criptomoedas',
+    },
+    kofi: {
+        header: 'Doar através do PayPal ou cartão de crédito/débito',
+        buttonText: 'Doar através do Ko-Fi',
+        message: 'Não é necessário criar uma conta para doar através do Ko-fi. Todas as doações vão directamente para o programador.',
+    },
+};
