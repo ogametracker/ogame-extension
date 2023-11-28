e2be0887fecf4758aec3326d3a4bdbdb94edf389
Rodrigo Correia <rodrigo@correia.red>
2023-11-28 14:53:36 +0000

    Translation - Ingame - PT-PT

---
+++ src/shared/i18n/extension/ingame/pt-pt.ts
@@ -0,0 +1,5 @@
+import { ExtensionIngameTranslations } from "./type";
+
+export const en: ExtensionIngameTranslations = {
+    tooltipNewTab: 'Abrir a interface do OGame Tracker numa nova tab.',
+};
