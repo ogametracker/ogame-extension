47d2380eac2038e8fd86c353ce41319d6caff76b
backfromexile <backfromexile@web.de>
2023-12-13 16:48:17 +0100

    fix user language detection being broken because OGame sets the user language to 'undefined' (as a string) if the player is in vmode

--- src/shared/ogame-web/getOgameMeta.ts
+++ src/shared/ogame-web/getOgameMeta.ts
@@ -18,8 +18,18 @@ function getLanguageMeta(): string {
     return (document.querySelector('meta[name="ogame-language"]') as HTMLMetaElement | null)?.content ?? _throw('Cannot find language meta tag');
 }
 
+function getUserLanguage(): string {
+    const userLanguage = getCookies()['oglocale'];
+    
+    if(userLanguage == null || userLanguage == 'undefined') { // yes, it's actually a string with 'undefined' for whatever reason
+        return getLanguageMeta();
+    }
+
+    return userLanguage;
+}
+
 export function getOgameMeta(): MessageOgameMeta {
-    const userLanguage = getCookies()['oglocale'] ?? getLanguageMeta();
+    const userLanguage = getUserLanguage;
 
     const serverText = (document.querySelector('meta[name="ogame-universe"]') as HTMLMetaElement | null)?.content ?? _throw('Cannot find universe meta tag');
     const [serverIdText, language] = serverText.split('.')[0].split('-').map(x => x.replace(/^s/, ''));
