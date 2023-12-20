57658182acd1baf84309ae56c995b21c607bd723
backfromexile <backfromexile@web.de>
2023-12-20 13:59:27 +0100

    add portuguese as language option

--- src/views/stats/components/settings/ExtensionLanguageSettings.vue
+++ src/views/stats/components/settings/ExtensionLanguageSettings.vue
@@ -39,10 +39,12 @@
         private langs: LanguageKey[] = [
             LanguageKey.de,
             LanguageKey.en,
+            LanguageKey['pt-pt'],
         ];
         private languageNames: Partial<Record<LanguageKey, string>> = {
             [LanguageKey.de]: 'Deutsch',
             [LanguageKey.en]: 'English',
+            [LanguageKey['pt-pt']]: 'Português',
         };
         private regions: Partial<Record<LanguageKey, Record<string, string>>> = {
             [LanguageKey.en]: {
